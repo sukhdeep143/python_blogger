c38e3bdc44201f6ae946ba14f98088110e05069d
Sukhdeep Singh <sukhdeepsingh141414@gmail.com>
2025-03-22 21:48:06 +0530

    loading added

---
+++ src/app/loading.tsx
@@ -0,0 +1,8 @@
+export default function LoadingSpinner() {
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <p className="text-lg font-semibold">Loading...</p>
+      </div>
+    );
+  }
+  
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,22 @@
 // import Image from "next/image";
 
 import Landing_page from "@/components/Landing_page";
+// import LoadingSpinner from "./loading";
+import { Suspense } from 'react'
 
 
 export default function Home() {
   return (
     //  This div is using for main background for the blogger we are going to create
     <main className=" " >
-      <Landing_page />
+      {/* <Suspense  fallback={<LoadingSpinner />}>
+      <Landing_page  />
+      <Suspense/> */}
+
+      <Suspense fallback={<p>Loading weather...</p>}>
+      <Landing_page  />
+
+      </Suspense>
     
 
    </main>
